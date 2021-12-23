593aa39272c8d41c2e46095d5c767de329300e55
Suraj Nadimpally <51845404+surajnadimpally@users.noreply.github.com>
2021-12-23 02:21:07 -0500

    Update server.php

--- server.php
+++ server.php
@@ -4,7 +4,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * @author   Suraj Nadimpally <nadimpallysuraj@gmail.com>
  */
 
 $uri = urldecode(
